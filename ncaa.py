47ddf333b45e445694ee7e69c0325ebe39a0826e
fatalfame <fatalfame@gmail.com>
2016-01-26 21:56:32 -0700

    getting closer with the output file on the script. now i can print 100 and something rows of keys.

--- ncaa.py
+++ ncaa.py
@@ -5,11 +5,12 @@ files = ('../Code/ncaa_machine_learning/inputs/arizona.csv', '../Code/ncaa_machi
 for file_names in files:
     input_file = open(file_names, 'rt')
     reader = csv.DictReader(input_file)
+    fieldnames = ['W-L%', 'PTS', 'SOS']
+    writer = csv.DictWriter(open('..//Code/ncaa_machine_learning/outputs/team_features.csv', 'wb'),
+                            fieldnames=fieldnames)
     for row in reader:
-        row = {k: v for k, v in row.iteritems()}
         print row
-        fieldnames = ['Rk', 'W-L%', 'SOS']
-        writer = csv.DictWriter(open('..//Code/ncaa_machine_learning/outputs/team_features.csv', 'wb'),
-                                fieldnames=row)
+        row = {k: v for k, v in row.iteritems()}
         writer.writeheader()
-        writer.writerow(row)
+    for key, value in row.items():
+        writer.writerow([key], [value])
