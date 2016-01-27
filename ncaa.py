7835937bd776dfbb52de48b81849b1823eb6b42e
fatalfame <fatalfame@gmail.com>
2016-01-26 21:13:58 -0700

    totally revamped py script and tried to simplify the feature extraction using dictreader and dictwriter.

--- ncaa.py
+++ ncaa.py
@@ -1,66 +1,15 @@
 import csv as csv
-import numpy as np
-import itertools
 
-with open('../Downloads/arizona.csv', 'rt') as f:    # Load in the csv file
-    reader = csv.reader(f)
-    next(reader)
-    offense_features = []
-    win_loss_features = []
-    defense_features = []
+files = ('../Code/ncaa_machine_learning/inputs/arizona.csv', '../Code/ncaa_machine_learning/inputs/duke.csv')
+
+for file_names in files:
+    input_file = open(file_names, 'rt')
+    reader = csv.DictReader(input_file)
     for row in reader:
-        rk = row[0]
-        season = row[1]
-        conf = row[2]
-        wins = row[3]
-        losses = row[4]
-        percentage = row[5]
-        srs = row[6]
-        sos = row[7]
-        offense = row[8]
-        defense = row[9]
-        ap_pre = row[10]
-        ap_high = row[11]
-        ap_final = row[12]
-        tournament = row[13]
-        coaches = row[14]
-        offense_features.append(offense)
-        defense_features.append(defense)
-        win_loss_features.append(percentage)
-    print 'az', offense_features
-    print 'az', defense_features
-    print 'az', win_loss_features
-with open('../Downloads/duke.csv', 'rt') as d:    # Load in the csv file
-    reader = csv.reader(d)
-    next(reader)
-    offense2_features = []
-    win_loss2_features = []
-    defense2_features = []
-    for row in reader:
-        rk = row[0]
-        season = row[1]
-        conf = row[2]
-        wins = row[3]
-        losses = row[4]
-        percentage = row[5]
-        srs = row[6]
-        sos = row[7]
-        offense = row[8]
-        defense = row[9]
-        ap_pre = row[10]
-        ap_high = row[11]
-        ap_final = row[12]
-        tournament = row[13]
-        coaches = row[14]
-        offense2_features.append(offense)
-        defense2_features.append(defense)
-        win_loss2_features.append(percentage)
-    print 'dk', offense2_features
-    print 'dk', defense2_features
-    print 'dk', win_loss2_features
-
-
-output = open('../Downloads/team_features.csv', "wb")
-mywriter = csv.writer(output)
-mywriter.writerow(offense_features)
-output.close()
+        row = {k: v for k, v in row.iteritems()}
+        print row
+        fieldnames = ['Rk', 'W-L%', 'SOS']
+        writer = csv.DictWriter(open('..//Code/ncaa_machine_learning/outputs/team_features.csv', 'wb'),
+                                fieldnames=row)
+        writer.writeheader()
+        writer.writerow(row)
