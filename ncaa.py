9035335020bcfe9f38ed3839432e573e019d800b
fatalfame <fatalfame@gmail.com>
2016-01-27 21:02:41 -0700

    made progress with writing the dictionary rows. still looking weird with my loop. i'm terrible with tabbing.

--- ncaa.py
+++ ncaa.py
@@ -5,12 +5,13 @@ files = ('../Code/ncaa_machine_learning/inputs/arizona.csv', '../Code/ncaa_machi
 for file_names in files:
     input_file = open(file_names, 'rt')
     reader = csv.DictReader(input_file)
-    fieldnames = ['W-L%', 'PTS', 'SOS']
+    fieldnames = ['W-L%', 'PTS', 'SOS', 'Season']
     writer = csv.DictWriter(open('..//Code/ncaa_machine_learning/outputs/team_features.csv', 'wb'),
                             fieldnames=fieldnames)
     for row in reader:
         print row
         row = {k: v for k, v in row.iteritems()}
         writer.writeheader()
-    for key, value in row.items():
-        writer.writerow([key], [value])
+        for value in row.iteritems():
+            writer.writerow({'Season': row['Season'], 'W-L%': row['W-L%'], 'PTS': row['PTS'], 'SOS': row['SOS']})
+
