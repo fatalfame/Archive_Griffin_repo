6fa59467c498629eff86acaf448f209a421a8def
fatalfame <fatalfame@gmail.com>
2016-01-29 23:56:51 -0700

    renamed ncaa.py to feature_extractor to give clarity as to what the script is doing.

--- feature_extractor.py
+++ feature_extractor.py
@@ -1,21 +1,21 @@
 import csv as csv
 
 az = '../Code/ncaa_machine_learning/inputs/arizona.csv'
-duke = '../Code/ncaa_machine_learning/inputs/duke.csv'
+Opp = '../Code/ncaa_machine_learning/inputs/az_state.csv'
 
 
-fieldnames = ['Season', 'W-L%', 'OFF', 'DEF', 'DukeSeason', 'DukeW-L', 'DukeOFF', 'DukeDEF']
+fieldnames = ['Season', 'W-L%', 'OFF', 'DEF', 'OppSeason', 'OppW-L', 'OppOFF', 'OppDEF']
 writer = csv.DictWriter(open('..//Code/ncaa_machine_learning/outputs/team_features.csv', 'wb'),
                         fieldnames=fieldnames)
 
 writer.writeheader()
 input_file = open(az, 'rt')
-input_file2 = open(duke, 'rt')
+input_file2 = open(Opp, 'rt')
 reader = csv.DictReader(input_file)
 reader2 = csv.DictReader(input_file2)
 for row in reader:
     writer.writerow({'Season': row['Season'], 'W-L%': row['W-L%'], 'OFF': row['OFF'], 'DEF': row['DEF']})
 for row in reader2:
-    writer.writerow({'DukeSeason': row['Season'], 'DukeW-L': row['W-L%'], 'DukeOFF': row['OFF'],
-                     'DukeDEF': row['DEF']})
+    writer.writerow({'OppSeason': row['Season'], 'OppW-L': row['W-L%'], 'OppOFF': row['OFF'],
+                     'OppDEF': row['DEF']})
 
