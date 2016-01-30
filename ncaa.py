0596c4cda026be936bc35e53d7defcf3342a941d
fatalfame <fatalfame@gmail.com>
2016-01-29 19:43:19 -0700

    do we want the features for each team in separate columns? because this commit does that.

--- ncaa.py
+++ ncaa.py
@@ -1,15 +1,21 @@
 import csv as csv
 
-files = ('../Code/ncaa_machine_learning/inputs/arizona.csv', '../Code/ncaa_machine_learning/inputs/duke.csv')
+az = '../Code/ncaa_machine_learning/inputs/arizona.csv'
+duke = '../Code/ncaa_machine_learning/inputs/duke.csv'
 
-fieldnames = ['W-L%', 'PTS', 'SOS', 'Season']
+
+fieldnames = ['Season', 'W-L%', 'OFF', 'DEF', 'DukeSeason', 'DukeW-L', 'DukeOFF', 'DukeDEF']
 writer = csv.DictWriter(open('..//Code/ncaa_machine_learning/outputs/team_features.csv', 'wb'),
                         fieldnames=fieldnames)
+
 writer.writeheader()
-for file_names in files:
-    input_file = open(file_names, 'rt')
-    reader = csv.DictReader(input_file)
-    for row in reader:
-        print row
-        writer.writerow({'Season': row['Season'], 'W-L%': row['W-L%'], 'PTS': row['PTS'], 'SOS': row['SOS']})
+input_file = open(az, 'rt')
+input_file2 = open(duke, 'rt')
+reader = csv.DictReader(input_file)
+reader2 = csv.DictReader(input_file2)
+for row in reader:
+    writer.writerow({'Season': row['Season'], 'W-L%': row['W-L%'], 'OFF': row['OFF'], 'DEF': row['DEF']})
+for row in reader2:
+    writer.writerow({'DukeSeason': row['Season'], 'DukeW-L': row['W-L%'], 'DukeOFF': row['OFF'],
+                     'DukeDEF': row['DEF']})
 
