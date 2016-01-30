cc4e82908494d3443ecf612d0528dd8a768b0541
Devin Jacobs <dbutonz@gmail.com>
2016-01-29 17:39:12 -0700

    Move `writeheader()` outside the for loops
    If we call `writeheader()` inside any for loop, then we will have
    multiple headers.

--- ncaa.py
+++ ncaa.py
@@ -5,13 +5,13 @@ files = ('../Code/ncaa_machine_learning/inputs/arizona.csv', '../Code/ncaa_machi
 fieldnames = ['W-L%', 'PTS', 'SOS', 'Season']
 writer = csv.DictWriter(open('..//Code/ncaa_machine_learning/outputs/team_features.csv', 'wb'),
                         fieldnames=fieldnames)
+writer.writeheader()
 for file_names in files:
     input_file = open(file_names, 'rt')
     reader = csv.DictReader(input_file)
     for row in reader:
         print row
         row = {k: v for k, v in row.iteritems()}
-        writer.writeheader()
         for value in row.iteritems():
             writer.writerow({'Season': row['Season'], 'W-L%': row['W-L%'], 'PTS': row['PTS'], 'SOS': row['SOS']})
 
