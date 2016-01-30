335f7e55c353030e8a2834cc8c79376f2bae9cdb
Devin Jacobs <dbutonz@gmail.com>
2016-01-29 17:35:02 -0700

    Move `writer` outside the for loop
    We will be using `writer` for the entire program, so it should be one
    of the first things we create.

--- ncaa.py
+++ ncaa.py
@@ -2,12 +2,12 @@ import csv as csv
 
 files = ('../Code/ncaa_machine_learning/inputs/arizona.csv', '../Code/ncaa_machine_learning/inputs/duke.csv')
 
+fieldnames = ['W-L%', 'PTS', 'SOS', 'Season']
+writer = csv.DictWriter(open('..//Code/ncaa_machine_learning/outputs/team_features.csv', 'wb'),
+                        fieldnames=fieldnames)
 for file_names in files:
     input_file = open(file_names, 'rt')
     reader = csv.DictReader(input_file)
-    fieldnames = ['W-L%', 'PTS', 'SOS', 'Season']
-    writer = csv.DictWriter(open('..//Code/ncaa_machine_learning/outputs/team_features.csv', 'wb'),
-                            fieldnames=fieldnames)
     for row in reader:
         print row
         row = {k: v for k, v in row.iteritems()}
