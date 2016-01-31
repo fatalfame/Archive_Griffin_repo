6473bc864a9ea4b6703b55f403a72adb04107bdc
fatalfame <fatalfame@gmail.com>
2016-01-30 23:01:29 -0700

    the ncaa project may not be the best example to study. i'm failing to keep interest because the data is spotty. i've taken a quick attempt to move on to some NBA data which is super rich and fairly easy to get to. i want to gather a list of players who have made the all star team each year, analyze their stats, and pick some features which i would think to be good predictors of being an all-star player. getting the data is kind of dumb. they only let you export 100 rows at a time and that would take a while. so i decided to make a scraper to pull the player data. i'm having some trouble with a page intercept popup questionaire which is derailing my scraper.

---
+++ nba.py
@@ -0,0 +1,54 @@
+from selenium import webdriver
+import lxml.html
+import time
+
+
+SETTINGS = {'needs_driver': True, }
+
+driver = webdriver.Firefox()
+START_URL = 'http://www.basketball-reference.com/play-index/psl_finder.cgi'
+PER_GAME = ".//*[@id='psl_finder']/table/tbody/tr[2]/td[1]/div[1]/div/div[2]/input"
+ACTIVE = ".//*[@id='psl_finder']/table/tbody/tr[2]/td[3]/div[2]/div/input[1]"
+STAR = ".//*[@id='as_single']/div/div/input[1]"
+OPTION = ".//*[@id='order_by']/option[1]"
+SORT = ".//*[@id='psl_finder']/table/tbody/tr[2]/td[4]/div[3]/div[3]/input"
+SEARCH = ".//*[@id='psl_finder']/table/tbody/tr[2]/td[4]/div[3]/div[4]/input"
+RESULTS = ".//*[@id='stats']/tbody/tr"
+NEXT = ".//*[@id='page_content']/div[3]/p[2]/a"
+
+
+def main(driver):
+    driver.implicitly_wait(10)
+    driver.get(START_URL)
+    driver.find_element_by_xpath(PER_GAME).click()
+    driver.find_element_by_xpath(ACTIVE).click()
+    driver.find_element_by_xpath(STAR).click()
+    driver.find_element_by_xpath(OPTION).click()
+    driver.find_element_by_xpath(SORT).click()
+    driver.find_element_by_xpath(SEARCH).click()
+    while True:
+        page_root = lxml.html.fromstring(driver.page_source)
+        table_rows = page_root.xpath(RESULTS)
+    for row in table_rows:
+        try:
+            if row.xpath('.//td[2]/a')[0].text:
+                player = row.xpath('.//td[2]/a')[0].text
+        except IndexError:
+            continue
+        season = row.xpath('.//td[3]')[0].text
+        age = row.xpath('.//td[4]')[0].text
+        mins = row.xpath('.//td[9]')[0].text
+        rebs = row.xpath('.//td[20]')[0].text
+        asst = row.xpath('.//td[21]')[0].text
+        pts = row.xpath('.//td[26]')[0].text
+        print player+'/', season+'/', age+'/', pts+'/', rebs+'/', asst+'/', mins
+    print 'looking for next page'
+    time.sleep(5)
+    driver.find_element_by_xpath(NEXT).click()
+    time.sleep(5)
+
+
+if __name__ == '__main__':
+    main(driver)
+
+
