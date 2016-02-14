a609724721d9945708d911e1ce7b226950b8e11d
fatalfame <fatalfame@gmail.com>
2016-02-14 14:56:34 -0700

    working on a way to solve the string to float conversion issue.

--- march_madness/ncaa_team_matrix.py
+++ march_madness/ncaa_team_matrix.py
@@ -11,16 +11,39 @@ from sklearn.feature_selection import RFE
 from sklearn import *
 
 
-players = '..//Code/personal/march_madness/team_stats.csv'
+players = '..//Code/Griffin_repo/march_madness/team_stats.csv'
 input_file = open(players, 'rb')
 reader = csv.DictReader(input_file)
 targets = []
 features = []
 x_2015 = []
 for row in reader:
-    x = row['W']
-    print type(x)
-    print x
+    print row['School']
+    print row['Season']
+    for k in row['W']:
+        row['W'] = float(row['W'])
+    print 'W', type(row['W'])
+    for k in row['L']:
+        row['L'] = float(row['L'])
+    print 'L', type(row['L'])
+    for k in row['Percent']:
+        row['Percent'] = float(row['Percent'])
+    print '%', type(row['Percent'])
+    for k in row['SOS']:
+        row['SOS'] = float(row['SOS'])
+    print 'SOS', type(row['SOS'])
+    for k in row['OFF']:
+        row['OFF'] = float(row['OFF'])
+    print 'OFF', type(row['OFF'])
+    for k in row['DEF']:
+        row['DEF'] = float(row['DEF'])
+    print 'DEF', type(row['DEF'])
+    for k in row['AP High']:
+        row['AP High'] = float(row['AP High'])
+    print 'AP High', type(row['AP High'])
+    for k in row['AP Final']:
+        row['AP Final'] = float(row['AP Final'])
+    print 'AP Final', type(row['AP Final'])
     if '2015-16' not in row['Season']:
         features.append([row['W'],
         row['L'],
