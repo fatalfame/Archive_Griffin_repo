ce98a7b0c149b1083980d5510071aead5e440bc1
fatalfame <fatalfame@gmail.com>
2016-02-15 13:05:31 -0700

    just having issues with the classifier targets/features.

--- march_madness/ncaa_team_matrix.py
+++ march_madness/ncaa_team_matrix.py
@@ -18,28 +18,31 @@ targets = []
 features = []
 x_2015 = []
 for row in reader:
-    x = row['OFF']
-    print row['School']
-    print row['Season']
+    # print row['School']
+    # print row['Season']
     row['W'] = float(row['W'])
-    print 'W', type(row['W'])
     row['L'] = float(row['L'])
-    print 'L', type(row['L'])
     row['Percent'] = float(row['Percent'])
-    print '%', type(row['Percent'])
     if row['SOS']:
             row['SOS'] = float(row['SOS'])
     else:
         row['SOS'] = None
-    print 'SOS', type(row['SOS'])
-    row['OFF'] = float(row['OFF'])
-    print 'OFF', type(row['OFF'])
-    row['DEF'] = float(row['DEF'])
-    print 'DEF', type(row['DEF'])
-    row['AP High'] = float(row['AP High'])
-    print 'AP High', type(row['AP High'])
-    row['AP Final'] = float(row['AP Final'])
-    print 'AP Final', type(row['AP Final'])
+    if row['OFF']:
+            row['OFF'] = float(row['OFF'])
+    else:
+        row['OFF'] = None
+    if row['DEF']:
+            row['DEF'] = float(row['DEF'])
+    else:
+        row['DEF'] = None
+    if row['AP High']:
+            row['AP High'] = float(row['AP High'])
+    else:
+        row['AP High'] = None
+    if row['AP Final']:
+            row['AP Final'] = float(row['AP Final'])
+    else:
+        row['AP Final'] = None
     if '2015-16' not in row['Season']:
         features.append([row['W'],
         row['L'],
@@ -71,10 +74,14 @@ def convert(val):
 
 # print 'features:', features
 # print 'targets:', targets
-
+print 'imade it here!'
 classifier = RandomForestClassifier()
+print 'made classifier'
 classifier.fit(features, targets)
+print 'fitting features'
 predictions = classifier.predict([i[1:] for i in x_2015])
+print 'making predictions'
 final_result = zip(predictions, x_2015)
+print 'zipping result'
 for row in sorted(final_result, reverse=True):
     print row
