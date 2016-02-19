0eb29ee1c1b8faed334f14fac241d7719a43915c
fatalfame <fatalfame@gmail.com>
2016-02-18 23:57:29 -0700

    cleaned up feature script so it can now make predictions. predictions are a little off now cuz the season isn't over yet so teams look worse than they really are.

--- march_madness/ncaa_team_matrix.py
+++ march_madness/ncaa_team_matrix.py
@@ -1,6 +1,4 @@
 import numpy as np
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis, QuadraticDiscriminantAnalysis
-from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import *
 import csv as csv
@@ -18,31 +16,19 @@ targets = []
 features = []
 x_2015 = []
 for row in reader:
-    # print row['School']
-    # print row['Season']
     row['W'] = float(row['W'])
     row['L'] = float(row['L'])
     row['Percent'] = float(row['Percent'])
     if row['SOS']:
             row['SOS'] = float(row['SOS'])
-    else:
-        row['SOS'] = None
     if row['OFF']:
             row['OFF'] = float(row['OFF'])
-    else:
-        row['OFF'] = None
     if row['DEF']:
             row['DEF'] = float(row['DEF'])
-    else:
-        row['DEF'] = None
     if row['AP High']:
             row['AP High'] = float(row['AP High'])
-    else:
-        row['AP High'] = None
     if row['AP Final']:
             row['AP Final'] = float(row['AP Final'])
-    else:
-        row['AP Final'] = None
     if '2015-16' not in row['Season']:
         features.append([row['W'],
         row['L'],
@@ -54,7 +40,8 @@ for row in reader:
         row['AP Final']])
         targets.append(row['NCAA'])
     else:
-        x_2015.append([row['W'],
+        x_2015.append([row['School'],
+        row['W'],
         row['L'],
         row['Percent'],
         row['SOS'],
@@ -72,16 +59,10 @@ def convert(val):
         val = None
         return val
 
-# print 'features:', features
-# print 'targets:', targets
-print 'imade it here!'
-classifier = RandomForestClassifier()
-print 'made classifier'
+
+classifier = RandomForestRegressor()
 classifier.fit(features, targets)
-print 'fitting features'
 predictions = classifier.predict([i[1:] for i in x_2015])
-print 'making predictions'
 final_result = zip(predictions, x_2015)
-print 'zipping result'
 for row in sorted(final_result, reverse=True):
     print row
